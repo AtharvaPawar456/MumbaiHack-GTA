f1df677628e27196271d601524eb63c8c6e2bd02
Atharva Prashant Pawar <67649944+AtharvaPawar456@users.noreply.github.com>
2024-10-28 09:54:52 +0530

    mobile app integrated

--- MoZoopFlaskApp/static/setup/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    let list = document.querySelector('#navbar-default');
-    let closeBtn = document.querySelector('#closeBtn');
-    let menuBtn = document.querySelector('#menuBtn');
-  
-    list.classList.toggle('hidden');
-    closeBtn.classList.toggle('hidden');
-    menuBtn.classList.toggle('hidden');
-  }
